9c5aae05c8bd457ed0d075a584fc4fa9661d2052
Anant Kumar <anantkumar012002@gmail.com>
2025-12-26 20:44:42 +0530

    feat(email): create sendEmail function for sending emails using Resend service

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,5 @@
 import Redis from "ioredis";
-import { sendEmail } from "./email/email";
+import { sendEmail } from "./email";
 
 const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
