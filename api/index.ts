fdb5763e46cbb76a0886dffc35366e9287b131b3
Anant Kumar <anantkumar012002@gmail.com>
2026-01-28 18:46:15 +0530

    chore: add @vercel/node dependency to package.json

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from "../app";
 
-export default app;
+export default async (req: VercelRequest, res: VercelResponse) => {
+    return app(req, res);
+};
